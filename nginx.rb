f3c1e24dd17db4fd797a127e7ef3f18780bec11e
swetonic <tim@onehub.com>
2020-03-25 17:43:07 -0700

    Revert "trying with openssl directory specified"
    This reverts commit 79e517893bbe92545267cf7ed8308e6b5d0fbc66.

--- nginx.rb
+++ nginx.rb
@@ -41,7 +41,6 @@ class Nginx < Formula
       "--http-scgi-temp-path=#{var}/cache/nginx/scgi_temp",
       "--lock-path=#{var}/lock/nginx.lock",
       "--pid-path=#{var}/run/nginx.pid",
-      "--with-openssl=/usr/local/ssl",
       "--with-pcre-jit",
       "--with-debug",
       "--with-ipv6",
