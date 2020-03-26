8619fa99bdc21c267f1f57e1b3e340151bef2590
swetonic <tim@onehub.com>
2020-03-25 17:25:10 -0700

    openssl

--- nginx.rb
+++ nginx.rb
@@ -41,7 +41,7 @@ class Nginx < Formula
       "--http-scgi-temp-path=#{var}/cache/nginx/scgi_temp",
       "--lock-path=#{var}/lock/nginx.lock",
       "--pid-path=#{var}/run/nginx.pid",
-      "--with-openssl=/usr/local/ssl",
+      "--with-openssl=./openssl",
       "--with-pcre-jit",
       "--with-debug",
       "--with-ipv6",
